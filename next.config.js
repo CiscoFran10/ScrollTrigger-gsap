cc72385e343c6a06a7afe2bcf37287e3f3579f75
Gabriel <85837405+CiscoFran10@users.noreply.github.com>
2023-03-28 11:36:04 -0300

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  images: {
+    domains: ["www.roomgpt.io", "images.unsplash.com"],
+  },
+};
+
+module.exports = nextConfig;
